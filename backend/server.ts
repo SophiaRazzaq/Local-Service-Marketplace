433695efe70d55de585e10aa93846c972bb45ede
Sophia <sophiarazzaqq@gmail.com>
2025-05-04 23:37:42 +0500

    fix cors on chrome

--- backend/server.ts
+++ backend/server.ts
@@ -15,7 +15,12 @@ import paymentRouter from "./routes/payments";
 
 const app = express();
 
-app.use(cors());
+app.use(
+	cors({
+		origin: ["http://localhost:3000"],
+		credentials: true,
+	}),
+);
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("tiny"));
